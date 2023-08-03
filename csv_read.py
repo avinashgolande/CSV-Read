c442830f0872694b2818824e04acc3d6f276c841
avinashgolande <64679442+avinashgolande@users.noreply.github.com>
2023-08-03 12:54:21 +0530

    Add files via upload

--- csv_read.py
+++ csv_read.py
@@ -17,3 +17,23 @@ import io
 
 df = pd.read_csv(io.BytesIO(uploaded['Social_Network_Ads.csv']))
 print(df)
+
+# import modules
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+# read the dataset
+df = pd.read_csv('Social_Network_Ads.csv')
+
+# get the locations
+X = df.iloc[:, :-1]
+y = df.iloc[:, -1]
+
+# split the dataset
+X_train, X_test, y_train, y_test = train_test_split(
+	X, y, test_size=0.05, random_state=0)
+
+X_train
+
+X_test
